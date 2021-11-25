4ed0ebcaf24623d71a5202895c2c7baaad65d9ce
jsdegithub <def_myself@qq.com>
2021-11-26 00:44:21 +0800

    axios_default_config_x-icode-not-in-indexjs

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-import axios from "axios";
-axios.defaults.headers["x-icode"] = "46A0A6ADED08D5A9";
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store.store}>
